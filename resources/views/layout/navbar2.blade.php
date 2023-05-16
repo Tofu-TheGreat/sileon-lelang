e60142d5b30c0580c5886f3e8cc7fe48ea5f5699
Aerozxz <arnoldarmawann@gmail.com>
2023-05-16 14:17:47 +0700

    salah

--- resources/views/layout/navbar2.blade.php
+++ resources/views/layout/navbar2.blade.php
@@ -39,7 +39,7 @@
                     <a class="nav-link" href="{{ route('logout.admin') }}">Log Out</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" data-bs-toggle="modal" data-bs-target="#exampleModal">Profile</a>
+                    <a class="nav-link" data-bs-toggle="modal" data-bs-target="#profil">Profile</a>
                 </li>
 
 
@@ -51,132 +51,150 @@
         </div>
     </div>
 </nav>
-
-<!-- Button trigger modal -->
-
-<!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade" id="profil" tabindex="-1" aria-labelledby="profilLabel" aria-hidden="true">
     <div class="">
         <div class="">
-
             <div class="container">
-
                 <div class="row d-flex justify-content-center">
-
                     <div class="col-md-7">
-
                         <div class="card p-3 py-4 border-0 modal-dialog ">
                             <div class="modal-content  border-0">
                                 <div class="modal-body ">
                                     <div class="text-center">
-                                        <img src="https://img.inews.co.id/media/600/files/networks/2022/05/25/2de52_son-heung-min.jpg"
-                                            width="200" class="rounded-circle">
+                                        <img src="https://i.pinimg.com/originals/fa/37/a9/fa37a99e5f740712b1941c847f1b45f7.jpg" width="180" height="140" class="rounded-circle">
                                     </div>
-
                                     <div class="text-center mt-3">
+                                    {{-- <span class="bg-secondary p-1 px-4 rounded text-white">Pro</span> --}}
                                         <h5 class="mt-2 mb-0">{{ auth()->user()->nama_lengkap }}</h5>
-                                        <span>{{ auth()->user()->level }}</span><br>
-                                        <span>{{ auth()->user()->email }}</span>
-
-
+                                        <span>{{ auth()->user()->level }}</span>
+                                    <div class="buttons">
+                                        <button class="btn btn-outline-primary px-4"data-bs-toggle="modal" data-bs-target="#detail">Edit Data</button>
+                                    </div>
                                     </div>
                                 </div>
                             </div>
-
-
-
-
-
                         </div>
-
                     </div>
-
                 </div>
-
             </div>
-
         </div>
     </div>
 </div>
-
+<div class="modal fade" id="detail" tabindex="-1" aria-labelledby="detailLabel" aria-hidden="true">
+    <div class="">
+        <div class="">
+            <div class="container">
+                <div class="row d-flex justify-content-center">
+                    <div class="col-md-7">
+                        <div class="card p-3 py-4 border-0 modal-dialog ">
+                            <div class="modal-content  border-0">
+                                <div class="modal-body ">
+                                    <div class="form-floating mb-3">
+                                        <input type="text" class="form-control">
+                                        <label for="floatingInput" aria-valuetext="{{ auth()->user()->username }}" >Username</label>
+                                    </div>
+                                    <div class="form-floating mb-3">
+                                        <input type="password" class="form-control">
+                                        <label for="floatingInput" aria-valuetext="{{ auth()->user()->password }}" >Password</label>
+                                    </div>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="detail">Kembali</button>
+                                    <button type="submit" class="btn btn-primary">Simpan</button>
+                                  </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 <style>
-    .card {
-        border: none;
+    body{
+    background:#eee;
+}
 
-        position: relative;
-        overflow: hidden;
-        border-radius: 8px;
-        cursor: pointer;
-    }
+.card{
+    border:none;
 
-    .card:before {
+    position:relative;
+    overflow:hidden;
+    border-radius:8px;
+    cursor:pointer;
+}
 
-        content: "";
-        position: absolute;
-        left: 0;
-        top: 0;
-        width: 4px;
-        height: 100%;
-        background-color: #c9aeff;
-        transform: scaleY(1);
-        transition: all 0.5s;
-        transform-origin: bottom
-    }
+.card:before{
 
-    .card:after {
+    content:"";
+    position:absolute;
+    left:0;
+    top:0;
+    width:4px;
+    height:100%;
+    background-color:#E1BEE7;
+    transform:scaleY(1);
+    transition:all 0.5s;
+    transform-origin: bottom
+}
 
-        content: "";
-        position: absolute;
-        left: 0;
-        top: 0;
-        width: 4px;
-        height: 100%;
-        background-color: #4824aa;
-        transform: scaleY(0);
-        transition: all 0.5s;
-        transform-origin: bottom
-    }
+.card:after{
 
-    .card:hover::after {
-        transform: scaleY(1);
-    }
+    content:"";
+    position:absolute;
+    left:0;
+    top:0;
+    width:4px;
+    height:100%;
+    background-color:#8E24AA;
+    transform:scaleY(0);
+    transition:all 0.5s;
+    transform-origin: bottom
+}
 
-
-    .fonts {
-        font-size: 11px;
-    }
-
-    .social-list {
-        display: flex;
-        list-style: none;
-        justify-content: center;
-        padding: 0;
-    }
-
-    .social-list li {
-        padding: 10px;
-        color: #8E24AA;
-        font-size: 19px;
-    }
+.card:hover::after{
+    transform:scaleY(1);
+}
 
 
-    .buttons button:nth-child(1) {
-        border: 1px solid #8E24AA !important;
-        color: #8E24AA;
-        height: 40px;
-    }
+.fonts{
+    font-size:11px;
+}
+.mt-2 mb-0{
 
-    .buttons button:nth-child(1):hover {
-        border: 1px solid #8E24AA !important;
-        color: #fff;
-        height: 40px;
-        background-color: #8E24AA;
-    }
+}
 
-    .buttons button:nth-child(2) {
-        border: 1px solid #8E24AA !important;
-        background-color: #8E24AA;
-        color: #fff;
-        height: 40px;
-    }
+.social-list{
+    display:flex;
+    list-style:none;
+    justify-content:center;
+    padding:0;
+}
+
+.social-list li{
+    padding:10px;
+    color:#8E24AA;
+    font-size:19px;
+}
+
+
+.buttons button:nth-child(1){
+       border:1px solid #8E24AA !important;
+       color:#8E24AA;
+       height:40px;
+}
+
+.buttons button:nth-child(1):hover{
+       border:1px solid #8E24AA !important;
+       color:#fff;
+       height:40px;
+       background-color:#8E24AA;
+}
+
+.buttons button:nth-child(2){
+       border:1px solid #8E24AA !important;
+       background-color:#8E24AA;
+       color:#fff;
+        height:40px;
+}
 </style>
